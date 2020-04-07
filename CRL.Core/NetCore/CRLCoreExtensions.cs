5905c2e31bbaebc5630113ff2c919c678d882749
hubro <hubro@163.com>
2020-04-07 15:43:33 +0800

    .

---
+++ CRL.Core/NetCore/CRLCoreExtensions.cs
@@ -0,0 +1,21 @@
+﻿#if NETSTANDARD
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+#endif
+
+namespace CRL.Core.NetCore
+{
+#if NETSTANDARD
+    public static class CRLCoreExtensions
+    {
+        public static void AddCRLCore(this IServiceCollection services, Action<ConfigBuilder> setupAction)
+        {
+            var configBuilder = new CRL.Core.ConfigBuilder();
+            setupAction(configBuilder);
+        }
+    }
+#endif
+}
---
+++ CRL/NetCore/CRLExtensions.cs
@@ -0,0 +1,21 @@
+﻿#if NETSTANDARD
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+#endif
+
+namespace CRL.NetCore
+{
+#if NETSTANDARD
+    public static class CRLExtensions
+    {
+        public static void AddCRL(this IServiceCollection services, Action<ISettingConfigBuilder> setupAction)
+        {
+            var builder = CRL.SettingConfigBuilder.CreateInstance();
+            setupAction(builder);
+        }
+    }
+#endif
+}
